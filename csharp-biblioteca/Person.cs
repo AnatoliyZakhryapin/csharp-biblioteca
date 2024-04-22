81bc722e868013d75254389c83a55e392b54473c
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-04-22 15:26:41 +0200

    ADD Class Person

---
+++ csharp-biblioteca/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Person
+    {
+        private string nome;
+        private string cognome;
+
+        // -----------
+        // Costruttore
+        // -----------
+
+        public Person(string nome, string cognome)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+        }
+
+        // ---------
+        // Get e Set
+        // ----------
+
+        public string Nome
+        {
+            get { return this.nome; }
+            set { this.nome = value; }
+        }
+
+        public string Cognome
+        {
+            get { return this.cognome; }
+            set { this.cognome = value; }
+        }
+    }
+}
