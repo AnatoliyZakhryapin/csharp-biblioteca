4290d2b07e841eb1f30c1e78a8d4a1e882f068f8
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-04-22 16:28:23 +0200

    ADD Class Prestito

---
+++ csharp-biblioteca/Prestito.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Prestito
+    {
+        private Person person;
+        private Documento documento;
+        private DateTime dal;
+        private DateTime all;
+
+        // -----------
+        // Costruttore
+        // -----------
+
+        public Prestito(Person person, Documento documento, DateTime dal, DateTime all)
+        {
+            this.person = person;
+            this.documento = documento;
+            this.dal = dal;
+            this.all = all;
+        }
+    }
+}
